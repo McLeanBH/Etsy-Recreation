98b13b39f06e899814eb613ea2b57930037d5b0b
McLeanBH <mcleanbh@gmail.com>
2015-01-23 15:19:38 -0500

    updated-getting-much-closer-now

--- main.js
+++ main.js
@@ -1,53 +1,33 @@
-// 
-// (function(){
-//   'use strict';
-//
-//   var chimps = rawChimpData.chimps;
-//
-//   $(document).ready(function(){
-//
-//     var $list = $('.chimps-list');
-//
-//     chimps.forEach(function(chimp){
-//       var chimpText = renderTemplate('chimp-item', {
-//         name: chimp.name,
-//         // brewery: beer.brewery.name
-//       });
-//       $list.append(chimpText);
-//     });
-//
-//   });
-//
-//   function renderTemplate(name, data) {
-//     var $template = $('[data-template-name=' + name + ']').text();
-//     $.each(data, function(prop, value) {
-//       $template = $template.replace('<% ' + prop + ' %>', value);
-//     });
-//     return $template;
-//   }
-//
-// })();
+(function(){
+  'use strict';
 
 
+  var chimps = rawChimpData.results; // figure out what i need here using the console.
 
+  $(document).ready(function(){ // runs the function on document ready
 
+    var $list = $('.chimps-list'); // targets ul with class chimp in the html
 
+    chimps.forEach(function(chimp){
+      var chimpText = renderTemplate('chimpResults', {
+        title: chimp.title,
+        price: chimp.price,
+        currency: chimp.currency_code,
+        description: chimp.description,
+        images: chimp.Images[0].url_fullxfull,
+        shop: chimp.Shop.shop_name,
+        url: chimp.url,
+      });
+      $list.append(chimpText);
+    });
 
+    function renderTemplate(name, data) {
+      var $template = $('[data-template-name=' + name + ']').text();
+      $.each(data, function(prop, value) {
+        $template = $template.replace('<% ' + prop + ' %>', value);
+      });
+      return $template;
+    }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-// $('.todo button').on('click', function(event){
-//     // add the class .complete to just that todo
-//     $(this).toggleClass('complete');
-//   });
+  });
+})();
